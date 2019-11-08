44fc80408996e6df5c800305aea7bba76a60739d
Marcial Cabrera <bladi22>
2019-11-08 09:28:14 -0600

    initial commit

---
+++ app/src/main/java/com/mvcsoftware/camera_mao_app_kt/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mvcsoftware.camera_mao_app_kt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
